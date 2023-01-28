b971b16be55f815e81ea201d88796f13c1b07f9a
francescobonacci <francescobonacci25@gmail.com>
2023-01-28 19:55:05 +0100

    add route function "search()" for query that return reviews which contains the word specified by user in frontend (file "app.py") add search functionality in frontend (file "index.html") add route function "cancelResearch()" that undoes the research by word in "app.py" add function "getCounting()" that return a counting of reviews showed in the page (file "app.py") add <script> tag that get the counting of showed reviews in "index.html" add modal for predict sentiment of reviews typed by user (file "index.html")

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import main
 
 # istruzioni terminale per avviare il progetto
@@ -15,22 +15,24 @@ orderedByShorter = False
 orderedByLonger = False
 filteredWithSpoilers = False
 filteredWithoutSpoilers = False
+searchString = ""
 
 @app.route('/')
 def index():
-    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     filteredByPositive = False
     filteredByNegative = False
     orderedByShorter = False
     orderedByLonger = False
     filteredWithoutSpoilers = False
     filteredWithSpoilers = False
+    searchString = ""
     currentReviews = main.reviews
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
 @app.route('/positive')
 def getPositive():
-    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     if (not filteredByPositive):
         currentReviews = main.filterByPositive(main.reviews)
     else:
@@ -47,11 +49,14 @@ def getPositive():
         currentReviews = main.filterByNoSpoilers(currentReviews)
     elif (filteredWithSpoilers):
         currentReviews = main.filterBySpoilers(currentReviews)
+    # controlliamo se era stato effettuata la ricerca per una determinata parola/frase
+    if (searchString != ""):
+        currentReviews = main.filterByWord(currentReviews, searchString)
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
 @app.route('/negative')
 def getNegative():
-    global currentReviews, filteredByNegative, filteredByPositive, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, filteredByNegative, filteredByPositive, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     if (not filteredByNegative):
         currentReviews = main.filterByNegative(main.reviews)
     else:
@@ -68,11 +73,14 @@ def getNegative():
         currentReviews = main.filterByNoSpoilers(currentReviews)
     elif (filteredWithSpoilers):
         currentReviews = main.filterBySpoilers(currentReviews)
+    # controlliamo se era stato effettuata la ricerca per una determinata parola/frase
+    if (searchString != ""):
+        currentReviews = main.filterByWord(currentReviews, searchString)
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
 @app.route('/shorter')
 def orderByShorterReviews():
-    global currentReviews, orderedByShorter, orderedByLonger, filteredByPositive, filteredByNegative, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, orderedByShorter, orderedByLonger, filteredByPositive, filteredByNegative, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     if (not orderedByShorter):
         currentReviews = main.orderByShortReviews(currentReviews)
     else:
@@ -82,13 +90,14 @@ def orderByShorterReviews():
         elif (filteredByNegative): currentReviews = main.filterByNegative(currentReviews)
         if (filteredWithoutSpoilers): currentReviews = main.filterByNoSpoilers(currentReviews)
         elif (filteredWithSpoilers): currentReviews = main.filterBySpoilers(currentReviews)
+        if (searchString != ""): currentReviews = main.filterByWord(currentReviews, searchString)
     orderedByShorter = not orderedByShorter
     orderedByLonger = False
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
 @app.route('/longer')
 def orderByLongerReviews():
-    global currentReviews, orderedByLonger, orderedByShorter, filteredByPositive, filteredByNegative, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, orderedByLonger, orderedByShorter, filteredByPositive, filteredByNegative, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     if (not orderedByLonger):
         currentReviews = main.orderByLongReviews(currentReviews)
     else:
@@ -98,13 +107,14 @@ def orderByLongerReviews():
         elif (filteredByNegative): currentReviews = main.filterByNegative(currentReviews)
         if (filteredWithoutSpoilers): currentReviews = main.filterByNoSpoilers(currentReviews)
         elif (filteredWithSpoilers): currentReviews = main.filterBySpoilers(currentReviews)
+        if (searchString != ""): currentReviews = main.filterByWord(currentReviews, searchString)
     orderedByLonger = not orderedByLonger
     orderedByShorter = False
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
 @app.route('/withoutSpoilers')
 def getReviewsWithoutSpoilers():
-    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     if (not filteredWithoutSpoilers):
         currentReviews = main.filterByNoSpoilers(main.reviews)
     else:
@@ -121,11 +131,14 @@ def getReviewsWithoutSpoilers():
         currentReviews = main.filterByPositive(currentReviews)
     elif (filteredByNegative):
         currentReviews = main.filterByNegative(currentReviews)
+    # controlliamo se era stato effettuata la ricerca per una determinata parola/frase
+    if (searchString != ""):
+        currentReviews = main.filterByWord(currentReviews, searchString)
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
 @app.route('/withSpoilers')
 def getReviewsWithSpoilers():
-    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers
+    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
     if (not filteredWithSpoilers):
         currentReviews = main.filterBySpoilers(main.reviews)
     else:
@@ -142,10 +155,39 @@ def getReviewsWithSpoilers():
         currentReviews = main.filterByPositive(currentReviews)
     elif (filteredByNegative):
         currentReviews = main.filterByNegative(currentReviews)
+    # controlliamo se era stato effettuata la ricerca per una determinata parola/frase
+    if (searchString != ""):
+        currentReviews = main.filterByWord(currentReviews, searchString)
     return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
 
+@app.route('/search')
+def search():
+    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
+    searchString = request.args.get('searchString')
+    currentReviews = main.filterByWord(currentReviews, searchString)
+    return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
+
+@app.route('/cancelResearch')
+def cancelResearch():
+    global currentReviews, filteredByPositive, filteredByNegative, orderedByShorter, orderedByLonger, filteredWithoutSpoilers, filteredWithSpoilers, searchString
+    searchString = ""
+    currentReviews = main.reviews
+    # controlliamo tutti i filtri e gli ordinamenti
+    if (filteredByPositive): currentReviews = main.filterByPositive(currentReviews)
+    elif (filteredByNegative): currentReviews = main.filterByNegative(currentReviews)
+    if (filteredWithoutSpoilers): currentReviews = main.filterByNoSpoilers(currentReviews)
+    elif (filteredWithSpoilers): currentReviews = main.filterBySpoilers(currentReviews)
+    if (orderedByShorter): currentReviews = main.orderByShortReviews(currentReviews)
+    elif (orderedByLonger): currentReviews = main.orderByLongReviews(currentReviews)
+    return render_template('index.html', reviews=currentReviews.take(6), flags=getFlags())
+
+@app.route('/counting')
+def getCounting():
+    counting = [currentReviews.count(), main.filterByPositive(currentReviews).count(), main.filterByNegative(currentReviews).count()]
+    return counting
+
 def getFlags():
-    return [filteredByPositive, filteredByNegative, filteredWithSpoilers, filteredWithoutSpoilers, orderedByShorter, orderedByLonger]
+    return [filteredByPositive, filteredByNegative, filteredWithSpoilers, filteredWithoutSpoilers, orderedByShorter, orderedByLonger, searchString]
 
 def stampaBooleani():
     print("positive: " + str(filteredByPositive))
